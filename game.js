8d5cc0073a8e99bac5864a0fbcaba519caf6490c
Dexeat <Dexeat@users.noreply.github.com>
2020-03-04 16:13:38 +0100

    update

--- game.js
+++ game.js
@@ -1,19 +1,60 @@
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
+    physics: {
+        default: 'arcade',
+    },
     scene: {
         preload: preload,
         create: create,
         update: update
-    },
-    width: 800,
-    height: 600
+    }
 };
+
+var player;
+var cursors;
 
 var game = new Phaser.Game(config);
 
-function preload() {}
+function preload ()
+{
+    this.load.image('bg', 'assets/fond/html-color-codes-color-tutorials-hero-00e10b1f.jpg');
+    this.load.image('block', 'assets/char/char.png');
+}
 
-function create() {}
+function create ()
+{
+    this.cameras.main.setBounds(0, 0, 1920 * 2, 1080 * 2);
+    this.physics.world.setBounds(0, 0, 1920 * 2, 1080 * 2);
 
-function update() {}
+    this.add.image(0, 0, 'bg').setOrigin(0);
+    //this.add.image(1920, 0, 'bg').setOrigin(0).setFlipX(true);
+    //this.add.image(0, 1080, 'bg').setOrigin(0).setFlipY(true);
+    //this.add.image(1920, 1080, 'bg').setOrigin(0).setFlipX(true).setFlipY(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    player = this.physics.add.image(400, 300, 'block');
+
+    player.setCollideWorldBounds(true);
+
+    this.cameras.main.startFollow(player, true, 0.05, 0.05);
+}
+
+function update ()
+{
+
+    player.setVelocity(0);
+    if (cursors.left.isDown){
+        player.setVelocityX(-500);
+    }
+    else if (cursors.right.isDown){
+        player.setVelocityX(500);
+    }
+    if (cursors.up.isDown){
+        player.setVelocityY(-500);
+    }
+    else if (cursors.down.isDown){
+        player.setVelocityY(500);
+    }
+}
